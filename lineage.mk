11cc006c4f08c50fdbfc79342b25e892a0466fdc
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2018-04-06 13:40:14 +0200

    rolex: Add build fingerprint
    Change-Id: I775c8eb6311502529360ef262b38f1c1ee9572cd

--- lineage.mk
+++ lineage.mk
@@ -30,4 +30,10 @@ PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := rolex
 PRODUCT_MANUFACTURER := xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="santoni" \
+    PRODUCT_NAME="santoni" \
+    BUILD_FINGERPRINT="Xiaomi/rolex/rolex:7.1.2/N2G47H/8.3.29:user/release-keys" \
+    PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H 8.3.29 release-keys"
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
