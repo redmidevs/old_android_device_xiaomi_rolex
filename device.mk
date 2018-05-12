fb846fd24d423ba85c3f0c3271d4d0ee92b8ed38
haran <harangovindan@gmail.com>
2018-05-12 11:55:53 +0200

    rolex: Binderize
    * Binderize Camera, Ir and Graphics Composer

--- device.mk
+++ device.mk
@@ -148,6 +148,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
@@ -156,7 +157,8 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
